c38772ed730f5e46dd5813b2534308ab95332ef0
BrunoMWorm <brunomatissek.worm@gmail.com>
2022-02-17 07:59:18 -0300

    ChoiceCalculus passing the Int of the choice as param.

--- ChoiceCalculusPlayground.hs
+++ ChoiceCalculusPlayground.hs
@@ -1,26 +1,21 @@
-import Data.List (elemIndex)
 -- Uma dimensão 'DimId' pode ter múltiplas tags 'TagId' representando possíveis escolhas
 type DimId = String
-type DimDef = (DimId, [TagId])
 type TagId = String
-
-indexOf::DimDef -> TagId -> Maybe Int
-indexOf(d,ts) t = elemIndex t ts
 
 -- Vamos construir um objeto ciente de variabilidade.
 data V a = Obj a -- Não precisamos retornar o objeto em uma estrutura de árvore somente retornamos a representação do objeto na Object Language
-    | Dim DimDef (V a) -- Definindo uma nova dimenção 'Dim' com nome 'DimId', as tags 'TagId' e uma expressão variacional
+    | Dim DimId [TagId] (V a) -- Definindo uma nova dimenção 'Dim' com nome 'DimId', as tags 'TagId' e uma expressão variacional
     | Chc DimId [V a] -- Para cada tag da dimensão 'DimId', apresentamos uma expressão 'V a' para escolha
     deriving (Show)
 
 -- Uma dimensão atômica é uma que limitamos o escopo a uma única decisão, e retornamos a expressão de cara logo.
--- A sacada: a expressão retornada por 'atomic' é justamente uma única expressão Chc
-atomic :: DimDef -> [V a] -> V a
-atomic (d,ts) cs = Dim (d,ts) $ Chc d cs
+-- A sagada: a expressão retornada por 'atomic' é justamente uma única expressão Chc
+atomic :: DimId -> [TagId] -> [V a] -> V a
+atomic d ts cs = Dim d ts $ Chc d cs
 
 -- Exercícios:
 dimA :: V a -> V a
-dimA = Dim ("A",["a1", "a2"])
+dimA = Dim "A" ["a1", "a2"]
 --
 chcA :: [V a] -> V a -- Note: uma construção de uma 'Choice' aceita uma lista de expressões e retorna apenas uma expressão "encodando" as choices
 chcA = Chc "A" -- Eu não teria que enfiar a tag em algum lugar?
@@ -28,13 +23,12 @@ chcA = Chc "A" -- Eu não teria que enfiar a tag em algum lugar?
 
 -- Choice Elimination
 -- Obs: veja que a TagId não serve pra nada...
-choiceElimination::V a -> DimId -> TagId -> V a
+choiceElimination::V a -> DimId -> Int -> V a
 choiceElimination (Obj a) _ _ = Obj a
-choiceElimination (Dim (d', ts') v') d t
-    | d' == d = Dim (d', ts') v'
-    | otherwise = Dim (d',ts') (choiceElimination v' d t)
+choiceElimination (Dim d' ts' v') d t
+    | d' == d = Dim d' ts' v'
+    | otherwise = Dim d' ts' (choiceElimination v' d t)
 choiceElimination (Chc d' vs) d t
-    | d' == d = choiceElimination (vs !! indexOf t) d t 
+    | d' == d = choiceElimination (vs !! t) d t 
     | otherwise = Chc d $ map (\v' -> choiceElimination v' d t) vs
 
-type Choice = (DimId, TagId)
